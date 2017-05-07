de7e67b93b54c7a95d4254074b27cf708bd05d01
Adron Hall <adronhall@gmail.com>
2017-05-07 12:15:56 -0700

    Adding the extra libs in the project.

---
+++ libraryOne/libraryTwo.go
@@ -0,0 +1,8 @@
+package libtwo
+
+func libraryTwoDisplayMessage(thisMessage string) (string) {
+	var s string
+	s = "The message is "
+	s += thisMessage
+	return s
+}
---
+++ libraryTwo/libraryOne.go
@@ -0,0 +1,5 @@
+package libone
+
+func libraryOneAddStuff (gimmeNumber float64) (float64) {
+	return gimmeNumber * 20
+}
